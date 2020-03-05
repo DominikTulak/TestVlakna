07a93e0c48b8d99564b8459ad301e4c1ba0227d3
unknown <Administrator@A211-16.ad.pslib.cz>
2020-03-05 12:57:00 +0100

    .

---
+++ PhotoEditor/Model/Functions.cs
@@ -0,0 +1,208 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace PhotoEditor.Model
+{
+    class Functions
+    {
+        public BitmapImage src;
+
+        int[,] array2D;
+
+        public void ProvestVSamostatnemVlakne(BitmapImage img) //Úprava zelené v samostatném vlákně (bod č.1)
+        {
+            new System.Threading.Thread((img) =>
+            {
+                Uprava_3(img);
+            }).Start();
+        }
+        public string PorovnatRychlostMetod() //Porovnání rychlosti 2 metod (v zadání bod č.3)
+        {
+            Stopwatch stopwatch1 = new Stopwatch();
+            Stopwatch stopwatch2 = new Stopwatch();
+            stopwatch1.Start();
+            //Metoda_1();
+            stopwatch1.Stop();
+
+            stopwatch2.Start();
+            //Metoda_2();
+            stopwatch2.Stop();
+
+            /*nebo Console.WriteLine*/ return ("Čas první metody: {0},\n čas druhé metody: {1},\n rozdíl: {2}", stopwatch1.Elapsed, stopwatch2.Elapsed, (stopwatch1.Elapsed > stopwatch2.Elapsed ? stopwatch1.Elapsed - stopwatch2.Elapsed : stopwatch2.Elapsed - stopwatch1.Elapsed));
+        }
+
+        public BitmapImage Uprava_4(BitmapImage img, int delitel) //Redukce barevné hloubky (bod č.5)
+        {
+            array2D = BitmapImageToArray2D(img);
+            for (int x = 0; x < array2D.GetLength(0); x++)
+            {
+                for (int y = 0; y < array2D.GetLength(1); y++)
+                {
+                    int soucet = (array2D[x, y] & 0xFF000000) / delitel;
+                    soucet = soucet >> 16;
+                    if (soucet > 255) soucet = 255;
+                    soucet = soucet << 16;
+                    array2D[x, y] = array2D[x, y] + soucet;
+                }
+            }
+            return ConvertWriteableBitmapToBitmapImage(Array2DToBitmapImage());
+        }
+        
+
+
+
+
+        public BitmapImage Uprava_1(BitmapImage img)
+        {
+            array2D = BitmapImageToArray2D(img);
+
+            for (int x = 0; x < array2D.GetLength(0); x++)
+            {
+                for (int y = 0; y < array2D.GetLength(1); y++)
+                {
+                    array2D[x, y] = array2D[x, y] & 0xFF0000;
+                }
+            }
+            return ConvertWriteableBitmapToBitmapImage(Array2DToBitmapImage());
+        }
+
+        public BitmapImage Uprava_2(BitmapImage img)
+        {
+            array2D = BitmapImageToArray2D(img);
+            int o_kolik = 50;
+            int cervena_zesileni = o_kolik << 16;
+
+            for (int x = 0; x < array2D.GetLength(0); x++)
+            {
+                for (int y = 0; y < array2D.GetLength(1); y++)
+                {
+                    int soucet = (array2D[x, y] & 0x00FF0000) + cervena_zesileni;
+                    soucet = soucet >> 16;
+                    if (soucet > 255) soucet = 255;
+                    soucet = soucet << 16;
+                    array2D[x, y] = array2D[x, y] + soucet;
+                }
+            }
+            return ConvertWriteableBitmapToBitmapImage(Array2DToBitmapImage());
+        }
+        public BitmapImage Uprava_3(BitmapImage img)
+        {
+            array2D = BitmapImageToArray2D(img);
+            int o_kolik = 50;
+            int zelena_zesileni = o_kolik << 16;
+
+            for (int x = 0; x < array2D.GetLength(0); x++)
+            {
+                for (int y = 0; y < array2D.GetLength(1); y++)
+                {
+                    int soucet = (array2D[x, y] & 0x0000FF00) + zelena_zesileni;
+                    soucet = soucet >> 16;
+                    if (soucet > 255) soucet = 255;
+                    soucet = soucet << 16;
+                    array2D[x, y] = array2D[x, y] + soucet;
+                }
+            }
+            return ConvertWriteableBitmapToBitmapImage(Array2DToBitmapImage());
+        }
+        
+
+        public int[,] BitmapImageToArray2D(BitmapImage img)
+        {
+            array2D = new int[img.PixelHeight, img.PixelWidth];
+
+            WriteableBitmap wb = new WriteableBitmap(img);
+            int width = wb.PixelWidth;
+            int height = wb.PixelHeight;
+            int bytesPerPixel = (wb.Format.BitsPerPixel + 7) / 8;
+            int stride = wb.BackBufferStride;
+            wb.Lock();
+            unsafe
+            {
+                byte* pImgData = (byte*)wb.BackBuffer;
+                int cRowStart = 0;
+                int cColStart = 0;
+                for (int row = 0; row < height; row++)
+                {
+                    cColStart = cRowStart;
+                    for (int col = 0; col < width; col++)
+                    {
+                        byte* bPixel = pImgData + cColStart;
+                        //bPixel[0] // Blue
+                        //bPixel[1] // Green
+                        //bPixel[2] // Red
+                        int pixel = bPixel[2]; //Red
+                        pixel = (pixel << 8) + bPixel[1]; //Green
+                        pixel = (pixel << 8) + bPixel[0]; //Blue
+                        array2D[row, col] = pixel;
+
+                        cColStart += bytesPerPixel;
+                    }
+                    cRowStart += stride;
+                }
+            }
+            return array2D;
+
+        }
+
+        public WriteableBitmap Array2DToBitmapImage()
+        {
+            WriteableBitmap wb = new WriteableBitmap(src);
+            int width = wb.PixelWidth;
+            int height = wb.PixelHeight;
+            int bytesPerPixel = (wb.Format.BitsPerPixel + 7) / 8;
+            int stride = wb.BackBufferStride;
+            wb.Lock();
+            unsafe
+            {
+                byte* pImgData = (byte*)wb.BackBuffer;
+                int cRowStart = 0;
+                int cColStart = 0;
+                for (int row = 0; row < height; row++)
+                {
+                    cColStart = cRowStart;
+                    for (int col = 0; col < width; col++)
+                    {
+                        byte* bPixel = pImgData + cColStart;
+
+                        bPixel[0] = (byte)((array2D[row, col] & 0xFF));// Blue
+                        bPixel[1] = (byte)((array2D[row, col] & 0xFF00) >> 8);// Green
+                        bPixel[2] = (byte)((array2D[row, col] & 0xFF0000) >> 16);// Red
+
+                        cColStart += bytesPerPixel;
+                    }
+                    cRowStart += stride;
+                }
+            }
+            wb.Unlock();
+            wb.Freeze();
+
+            return wb;
+
+        }
+        public BitmapImage ConvertWriteableBitmapToBitmapImage(WriteableBitmap wbm)
+        {
+            BitmapImage bmImage = new BitmapImage();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                PngBitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(wbm));
+                encoder.Save(stream);
+                bmImage.BeginInit();
+                bmImage.CacheOption = BitmapCacheOption.OnLoad;
+                bmImage.StreamSource = stream;
+                bmImage.EndInit();
+                bmImage.Freeze();
+            }
+            return bmImage;
+        }
+
+        
+
+
+    }
+}
---
+++ PhotoEditor/ViewModel/MainViewModel.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+using PhotoEditor.Model;
+
+namespace PhotoEditor.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private BitmapImage picture;
+
+        public Command LoadCommand { get; private set; }
+        public Command Edit_1Command { get; private set; }
+        public Command Edit_2Command { get; private set; }
+        public Command Edit_3Command { get; private set; }
+        public Command Edit_4Command { get; private set; }
+
+        private Functions fnc;
+        
+        public MainViewModel()
+        {
+            LoadCommand = new Command(LoadCommand_Execute);
+            Edit_1Command = new Command(Edit_1Command_Execute);
+            Edit_2Command = new Command(Edit_2Command_Execute);
+            Edit_3Command = new Command(Edit_3Command_Execute);
+            Edit_4Command = new Command(Edit_4Command_Execute);
+
+            fnc = new Functions();
+        }
+
+        public BitmapImage Picture
+        {
+            get { return picture; }
+            set
+            {
+                picture = value;
+                ChangeProperty("Picture");
+            }
+        }
+        private void Edit_1Command_Execute()
+        {
+            if (Picture != null)
+            {
+                Picture = fnc.Uprava_1(Picture);
+            }
+            else
+            {
+                MessageBox.Show("No picture!");
+            }
+        }
+        private void Edit_2Command_Execute()
+        {
+            if (Picture != null)
+            {
+                Picture = fnc.Uprava_2(Picture);
+            }
+            else
+            {
+                MessageBox.Show("No picture!");
+            }
+        }
+        private void Edit_3Command_Execute()
+        {
+            if (Picture != null)
+            {
+                Picture = fnc.Uprava_3(Picture);
+            }
+            else
+            {
+                MessageBox.Show("No picture!");
+            }
+        }
+        private void Edit_4Command_Execute()
+        {
+            if (Picture != null)
+            {
+                Picture = fnc.Uprava_4(Picture);
+            }
+            else
+            {
+                MessageBox.Show("No picture!");
+            }
+        }
+
+        private void LoadCommand_Execute()
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.DefaultExt = ".jpg";
+            dlg.Filter = "Obrázek ().jpg |*.jpg| Všechny soubory| *.*";
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+                picture = new BitmapImage();
+                picture.BeginInit();
+                picture.UriSource = new Uri(filename, UriKind.Relative);
+                picture.CacheOption = BitmapCacheOption.OnLoad;
+                picture.EndInit();
+                fnc.src = picture;
+
+                ChangeProperty("Picture");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void ChangeProperty(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
